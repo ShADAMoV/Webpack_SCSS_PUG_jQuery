3971b77829870d8a34b08c93bb6b262fcee2c862
ShAD <84129656+ShADAMoV@users.noreply.github.com>
2022-04-25 20:00:03 +0300

    Webpack Configuration
    Just use "npm init", after that u can use "npm start" or "npm run build" :)

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import './styles/index.scss';
+import $ from 'jquery';
+import 'bootstrap';
+
+$('.block').html('jquery is working');
+
+constUserSrack = {
+    language: 'JavaScript',
+    framework: 'Angular'
+};
+
+const user = {
+    name: 'Vitalij',
+    age: '37',
+    ...userStack
+};
+
+
+
+console.log(user)
